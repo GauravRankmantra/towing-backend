b054fcc9887f567662c26a50eda103cea8b9af13
GauravRankmantra <gauravlakherarankmantra@gmail.com>
2025-07-01 15:29:17 +0530

    added health check

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const zipRouter = require("./zipCodeRoutes.js");
 const contactRouter = require("./contactRoutes.js")
 const bulkRouter=require("./bulkUpload.js")
+const healthRouter=require("./healthCheck.js")
 
 router.use("/zip", zipRouter);
 router.use("/contact",contactRouter)
 router.use("/bulk",bulkRouter)
+router.use("/health",healthRouter)
 
 module.exports = router;
